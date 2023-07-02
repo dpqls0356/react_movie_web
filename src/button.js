07fb6c50576c5219197634833eaaca3f18f33b6d
dpqls0356 <dpqls0356@gmail.com>
2023-07-02 23:55:25 +0900

    useEffect 에서 return역할

--- src/button.js
+++ src/button.js
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./Button.module.css";
-function Button({text,onClick}){
-    return <button onClick={onClick}className={styles.btn}>{text}</button>
+function Button({showing,onClick}){
+    return <button onClick={onClick} className={styles.btn}>{showing?"hide":"show"}</button>
 }
 Button.prototype={
     text:PropTypes.string.isRequired,
