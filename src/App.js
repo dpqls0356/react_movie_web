283cb029122730c92c216fa5dae628a6411efba4
dpqls0356 <dpqls0356@gmail.com>
2023-07-10 18:36:32 +0900

    Todo list 만들기

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import { useEffect, useState } from "react";
-import Button from "./button";
 import styles from "./App.module.css";
-function Hello(){
-  useEffect(()=>{
-    console.log("create");
-    return () => console.log("destroyed");
-  },[]);
-  return (
-    <h1>Hello</h1>
-  );
-}
+
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () =>setShowing((current)=> !current);
+  const [toDo, setToDo] = useState();
+  const [toDos,setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo===""){
+      return;
+    }
+    else{
+      setToDos((currentArray) => [toDo,...currentArray]);
+      setToDo("");
+    }
+  };
+  const onClickAndRemove = (index) =>{
+    // event.target.parentElement.remove();
+    // index와 todoIndex가 같지않으면 return
+    // setToDos(toDos.filter((item, todoIndex) => index !== todoIndex));
+    setToDos((curToDos)=>curToDos.filter((item,toDoIndex)=> toDoIndex!==index));
+  }
   return (
     <div >
-      {showing?<Hello/>:null}
-      <Button showing={showing} onClick={onClick}/>
+      <h1>MY TODO LIST [ {toDos.length} ]</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange = {onChange} value={toDo || ""} type="text" placeholder="Wirte your to do.."></input>
+        <button>Add To Do</button>
+      </form>
+      <hr/>
+      <ul>
+        {toDos.map((item,index)=><li key={index}>{item}<button onClick={()=>onClickAndRemove(index)}>x</button></li>)}
+      </ul>
     </div>
   );
 }
