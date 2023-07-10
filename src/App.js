6b04eefc2fc78ff59c33f91a7528c0e6c834c32d
dpqls0356 <dpqls0356@gmail.com>
2023-07-10 18:59:07 +0900

    coin api 사용해 가격 알려주는 사이트 만들기

--- src/App.js
+++ src/App.js
@@ -2,35 +2,20 @@ import { useEffect, useState } from "react";
 import styles from "./App.module.css";
 
 function App() {
-  const [toDo, setToDo] = useState();
-  const [toDos,setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo===""){
-      return;
-    }
-    else{
-      setToDos((currentArray) => [toDo,...currentArray]);
-      setToDo("");
-    }
-  };
-  const onClickAndRemove = (index) =>{
-    // event.target.parentElement.remove();
-    // index와 todoIndex가 같지않으면 return
-    // setToDos(toDos.filter((item, todoIndex) => index !== todoIndex));
-    setToDos((curToDos)=>curToDos.filter((item,toDoIndex)=> toDoIndex!==index));
-  }
+  const [loading,setLoading] = useState(true);
+  const [coins,setCoins] = useState([]);
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers").then((response)=>response.json()).then((json)=>{
+      setCoins(json);
+      setLoading(false);
+    });
+  },[])
   return (
-    <div >
-      <h1>MY TODO LIST [ {toDos.length} ]</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange = {onChange} value={toDo || ""} type="text" placeholder="Wirte your to do.."></input>
-        <button>Add To Do</button>
-      </form>
-      <hr/>
+    <div>
+      <h1>The Coins! {coins.length}</h1>
+      {loading?<strong>Loading</strong>:null}
       <ul>
-        {toDos.map((item,index)=><li key={index}>{item}<button onClick={()=>onClickAndRemove(index)}>x</button></li>)}
+        {coins.map((item)=><li key={item.id}>{item.name} ({item.symbol}) : {item.quotes.USD.price}USD</li>)}
       </ul>
     </div>
   );
