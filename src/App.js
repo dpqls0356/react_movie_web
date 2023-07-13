77648c09be0df757332b2609c6e11b8b899c8792
dpqls0356 <dpqls0356@gmail.com>
2023-07-13 21:09:59 +0900

    파일 정리

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route,Link } from "react-router-dom";
-import styles from "./App.module.css";
-import Home from "./routers/Home";
-import Detail from "./routers/Detail";
-function App() {
- return <Router>
-  <Routes>
-    <Route path={process.env.PUBLIC_URL +"/movie/:id"} element={<Detail/>}/>
-    <Route path={process.env.PUBLIC_URL +"/"} element={<Home/>} />
-  </Routes>
- </Router>
+import React from 'react';
+
+function App(){
+    return (
+        <div></div>
+    );
 }
 
 export default App;
